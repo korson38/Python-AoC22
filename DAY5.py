3d46b4a5e755922491283e059b58c3cdc1ef9804
korson38 <129417028+korson38@users.noreply.github.com>
2023-05-22 12:49:14 +0200

    Update DAY5.py

--- DAY5.py
+++ DAY5.py
@@ -1,7 +1,7 @@
+# preparing data to process 
 with open("DAY5", "r")as file:
     plik = file.read().split("\n\n")
     crates_lth = len(plik[0].split("\n")[-1][1::4])
-    crates_hgt = len(plik[0].split("\n"))-1
     crates = [[] for _ in range(crates_lth)]
     clean_crates = [[] for _ in range(crates_lth)]
     text = plik[0][1::4]
@@ -15,6 +15,7 @@ with open("DAY5", "r")as file:
     my_list = []
     crates = [crate[::-1] for crate in crates]
     l = 0
+# cleaning crates of empty spots
     for k in crates:
         for j in k:
             if j != ' ':
@@ -22,16 +23,17 @@ with open("DAY5", "r")as file:
         clean_crates[l] = my_list
         my_list = []
         l += 1
+# moving things from crates to crates
 moves = plik[1].split("\n")
 for line in moves:
     token = line.split(" ")
     qt, sce, dst = map(int, [token[1], token[3], token[5]])
     sce -= 1
     dst -= 1
-
     for _ in range(qt):
         rmv = clean_crates[sce].pop()
         clean_crates[dst].append(rmv)
+# taking tops of crates
 answer = ''
 for crate in clean_crates:
     answer += crate[-1]
